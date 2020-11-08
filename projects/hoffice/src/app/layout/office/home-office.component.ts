6418a7f62368752cef8bbf4da7438c40b4d06d19
nitichote <chotemail@yahoo.com>
2020-11-08 21:05:56 +0700

    e2

--- projects/hoffice/src/app/layout/office/home-office.component.ts
+++ projects/hoffice/src/app/layout/office/home-office.component.ts
@@ -10,6 +10,12 @@ export class HomeOfficeComponent implements OnInit {
   constructor(private ps: HofficeService) {}
 offices=[];
 amps=[];
+hoss=[];
+getHosInAmps(ampcode:string){
+  this.hoss=this.offices.filter(x=>x.ampcode===ampcode);
+  console.log("hoss=",this.hoss);
+  
+}
   ngOnInit(): void {
     this.ps.getReport('2').then((x) => {
       this.offices = x["message"];
@@ -20,8 +26,11 @@ amps=[];
         ampcode:x.ampcode,ampname:x.ampname}
       }
         );
-        this.amps = [...new Set(a)]   ;    
-console.log("amps=",this.amps);
+      
+this.amps = Object.values(a.reduce(function(r, e) {
+  let ampcode = e.ampcode, ampname = e.ampname;
+  return r[`${ampcode}|${ampname}`] = {ampcode, ampname}, r
+}, {}));
 
     });
   }
