b0f2f6459263e197f50438d61bed517874007702
nitichote <chotemail@yahoo.com>
2020-11-09 14:37:44 +0700

    e3

--- projects/hoffice/src/app/layout/office/home-office.component.ts
+++ projects/hoffice/src/app/layout/office/home-office.component.ts
@@ -1,38 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { HofficeService } from "../../service/hoffice_service";
 @Component({
-  selector: 'app-home-office',
-  templateUrl: './home-office.component.html',
-  styleUrls: ['./home-office.component.scss']
+  selector: "app-home-office",
+  templateUrl: "./home-office.component.html",
+  styleUrls: ["./home-office.component.scss"],
 })
 export class HomeOfficeComponent implements OnInit {
-
   constructor(private ps: HofficeService) {}
-offices=[];
-amps=[];
-hoss=[];
-getHosInAmps(ampcode:string){
-  this.hoss=this.offices.filter(x=>x.ampcode===ampcode);
-  console.log("hoss=",this.hoss);
-  
-}
+  offices = [];
+  amps = [];
+  hoss = [];
+  getHosInAmps(ampcode: string) {
+    this.hoss = this.offices.filter((x) => x.ampcode === ampcode);
+    console.log("hoss=", this.hoss);
+  }
+  getOfficeDetail(off_id) {
+    console.log("off=", off_id);
+  }
   ngOnInit(): void {
-    this.ps.getReport('2').then((x) => {
+    this.ps.getReport("2").then((x) => {
       this.offices = x["message"];
       console.log(this.offices);
-      
-     let a =this.offices.map(x=>{
-        return {
-        ampcode:x.ampcode,ampname:x.ampname}
-      }
-        );
-      
-this.amps = Object.values(a.reduce(function(r, e) {
-  let ampcode = e.ampcode, ampname = e.ampname;
-  return r[`${ampcode}|${ampname}`] = {ampcode, ampname}, r
-}, {}));
 
+      let a = this.offices.map((x) => {
+        return {
+          ampcode: x.ampcode,
+          ampname: x.ampname,
+        };
+      });
+
+      this.amps = Object.values(
+        a.reduce(function (r, e) {
+          let ampcode = e.ampcode,
+            ampname = e.ampname;
+          return (r[`${ampcode}|${ampname}`] = { ampcode, ampname }), r;
+        }, {})
+      );
     });
   }
-
 }
